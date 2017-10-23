4434318866c91503c9d67156f22d9b8ff99444a5
edhowland <ed.howland@gmail.com>
2017-10-23 18:04:38 -0400

    \'added simple ByteCodes class in bytecode.rb . Use .codes= to set them, and .next to get the next one\'

---
+++ bytecode.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# bytecode.rb - class ByteCode - collection of individual bytecodes
+# attr: pc : The current increment - Can be set for looping/branching, .etc
+#  :codes : The list of bytecodes
+# methods: :next : returns next element from codes
+
+
+class ByteCodes
+  def initialize
+    @codes = []
+    @pc = 0
+  end
+  attr_accessor :pc, :codes
+
+  def next
+    code = @codes[@pc]
+    @pc += 1
+    code
+  end
+end
+
