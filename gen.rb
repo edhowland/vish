80b399a7ead39090c690f437fbc7bc6dcfa4b334
edhowland <ed.howland@gmail.com>
2017-10-26 15:57:41 -0400

    \'simple working example of Parslet class Mini. Use Mini.new.parse 123 \'

---
+++ gen.rb
@@ -0,0 +1,12 @@
+# gen.rb - generates a parser from our grammar
+
+  require 'parslet' 
+
+class Mini < Parslet::Parser
+  rule(:integer) { match('[0-9]').repeat(1) }
+  root(:integer)
+end
+
+# Mini.new.parse("132432")  # => "132432"@0
+
+
