c850df9caaa435921301ab834073edbcc8f7b071
edhowland <ed.howland@gmail.com>
2017-10-27 11:38:24 -0400

    \'got mini.rb working with either of these examples: 1 , or 1+2. Will spit a intermediate AST as a hash where nodes are labeled with ther types\'

--- mini.rb
+++ mini.rb
@@ -5,19 +5,18 @@
 
 class Mini < Parslet::Parser
   # single character rules
-
   rule(:lparen)     { str('(') >> space? }
   rule(:rparen)     { str(')') >> space? }
     rule(:comma)      { str(',') >> space? }
 
   rule(:integer) { match('[0-9]').repeat(1).as(:int) >> space? }
-  rule(:identifier) { match['a-z'].repeat(1) }
+  rule(:identifier) { match('[a-z]').repeat(1) }
 
   rule(:space) { match('\s').repeat(1) }
   rule(:space?) { space.maybe }
 
   rule(:oper) { match('[+]') >> space? }
-  rule(:sum) { integer >> oper >> expr }
+  rule(:sum) { integer.as(:left) >> oper.as(:op) >> expr.as(:right) }
   rule(:expr) { sum | integer }
 
   root(:expr)
