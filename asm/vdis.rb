58a404ceabc157e46020906e3555355a215c8ee7
edhowland <ed.howland@gmail.com>
2017-10-30 17:38:33 -0400

    \'chckpt on vdis. Uses the new syntax to reload the Context object. MUST TODO: implement the rules in the vasm_grammar.rb\'

--- asm/vdis.rb
+++ asm/vdis.rb
@@ -13,6 +13,12 @@ bc, ctx = load_codes(File.open(fin, 'r'))
 out = File.open(fout, 'w')
 out.puts "# #{ARGV[0]} disassembly into #{ARGV[1]}"
 
+
+# write out the ctx : Context struct , sans the stack
+out.puts "constants: #{ctx.constants.join(',')}"
+out.puts "vars:"
+ctx.vars.each_pair {|k, v| out.puts "  #{k}=#{v}" }
+out.puts "# bytecodes"
 enumr = bc.codes.each
 begin
   loop do
