27390bdbfc2f28d29ae43d6daac4caaa247df48b
edhowland <ed.howland@gmail.com>
2017-10-31 16:43:42 -0400

    \'made the vasm.rb create the ctx variable correctly\'

--- asm/vasm.rb
+++ asm/vasm.rb
@@ -2,6 +2,14 @@
 # vasm.rb - assembles .vasm files into .vshc bytecode files
 
 require_relative 'vasm_grammar'
+require_relative 'utilities'
+require_relative 'vasm_transform'
+require_relative '../context'
+require_relative '../bytecode'
+
+
+
+
 
 
 fin, fout = ARGV
@@ -11,7 +19,14 @@ parser = VasmGrammar.new
 
 input = File.read(fin)
 begin
-  p parser.parse(input)
+  ir =  parser.parse(input)
+  tr = VasmTransform.new
+  im = tr.apply(ir)
+  ctx = Context.new
+
+  ctx.constants = im[:ctx][:constants]
+  ctx.vars = im[:ctx][:vars][:vlist].to_h
+  p ctx
 rescue Parslet::ParseFailed => failure
   puts failure.parse_failure_cause.ascii_tree  
 end
