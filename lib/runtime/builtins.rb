e1a7cf3e2288110e15498c5bd0342884e0e7d95a
edhowland <ed.howland@gmail.com>
2017-11-20 20:49:19 -0500

    \'added break, BreakCalled runtime error for eventual use in loop construct.\'

--- lib/runtime/builtins.rb
+++ lib/runtime/builtins.rb
@@ -1,8 +1,14 @@
 # builtins.rb - module Builtins - builtin methods
 
 
+class BreakCalled < RuntimeError; end
+
 module Builtins
   def self.echo(*args)
     args.map(&:to_s).join(' ') + "\n"
+  end
+
+  def self.break
+    raise BreakCalled.new
   end
 end
