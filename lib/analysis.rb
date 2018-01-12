6327c5cb947e49e67e9c78640d9af3a465728009
edhowland <ed.howland@gmail.com>
2018-01-12 17:59:23 -0500

    \'renamed lib/analysis/extract_assign_blocks.rb to lib/analysis/get_assign_to_blocks.rb . removed that unused method\'

--- lib/analysis.rb
+++ lib/analysis.rb
@@ -9,7 +9,7 @@ require_relative 'analysis/resolve_logical_and'
 
 
 require_relative 'analysis/match_subtree'
-require_relative 'analysis/extract_assign_blocks'
+require_relative 'analysis/get_assign_to_blocks'
 require_relative 'analysis/extract_lambdas'
 require_relative 'analysis/extract_functions'
 require_relative 'analysis/fixup_returns'
