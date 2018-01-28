5efe07ee3d36da2112457d5d6b5397f74b4b600c
edhowland <ed.howland@gmail.com>
2018-01-28 17:52:26 -0500

    \'removed some more TODO comments\'

--- lib/ast/loop_entry.rb
+++ lib/ast/loop_entry.rb
@@ -2,7 +2,7 @@
 
 class LoopEntry < Terminal
   attr_reader :loop_frame, :jump_to
-  # TODO: emit a :frame which pushes activation frame on ctx.call_stack
+
   def emit(bc, ctx)
     bc.codes << :frame
     @loop_frame = LoopFrame.new
