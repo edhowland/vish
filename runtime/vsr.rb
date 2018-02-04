b6895f60ede4f4fd2fa63739f9f6a3de63e7ad1a
edhowland <ed.howland@gmail.com>
2018-02-04 16:57:35 -0500

    \'updated runtime/vsr.rb with -r, --require flag. Updated README.md , Changelog with notes wrt that.\'

--- runtime/vsr.rb
+++ runtime/vsr.rb
@@ -9,6 +9,11 @@ require_relative '../lib/vish'
 opt=OptionParser.new do |o|
   o.banner = "vsr - Runtime for compiled Vish symcode files: *.vsc\nUsage: vsr myfile.vsc"
   o.separator ''
+  o.on('-r file', '--require file', String, 'Require file before executing runtime') do |file|
+    require file
+  end
+  o.separator ''
+
   o.on('-h', '--help', 'Display this help') do
     puts o
     exit(0)
@@ -18,6 +23,7 @@ opt=OptionParser.new do |o|
     exit(0)
   end
 end
+
 opt.parse!
 file = File.open(ARGV.first, 'r')
 bc, ctx = load_codes file
