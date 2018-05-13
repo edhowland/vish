5bf02ceba51a7087432e0edcad05c69eee125088
edhowland <ed.howland@gmail.com>
2018-05-13 00:16:23 -0400

    \'removed runtime/crystal code\'

--- runtime/crystal/runtime/lambda_not_found.cr
+++
@@ -1,8 +0,0 @@
-# lambda_not_found.rb - class LambdaNotFound < RuntimeError
-# raised if there is no actual Lambda body to jump to
-
-class LambdaNotFound < VishRuntimeError
-  def initialize name
-    super "Lambda not found: #{name}"
-  end
-end
--- runtime/crystal/runtime/stdlib.cr
+++
@@ -1,5 +0,0 @@
-# stdlib.rb - returns expanded path to internal ./std/lib.vs
-
-def stdlib
-  File.expand_path(File.dirname(__FILE__) + '/../../std/lib.vs')
-end
