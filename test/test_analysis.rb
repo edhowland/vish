ece3f643a6d299e599fedfd213208c3e25e47d5f
edhowland <ed.howland@gmail.com>
2018-09-14 19:24:07 -0400

    \'added more comprehensive tests for constant folding with recursive sub-expressions\'

--- test/test_analysis.rb
+++ test/test_analysis.rb
@@ -1,4 +1,4 @@
-# test_analysis.rb - tests for
+# test_analysis.rb - tests forconstant folding, tail code optimizations, others
 
 require_relative 'test_helper'
 
@@ -28,5 +28,18 @@ class TestAnalysis < BaseSpike
   def test_depth_of_simple_constant_expression_is
     compile '1+3'
     assert_eq 3, depth(@compiler.ast)
+  end
+  # multiple sub-expressions wherein all the exprs are constant
+  def test_multiple_sub_expressions
+    secs_in_year = compile '60*60*24*7*52'
+    assert_eq 3, depth(@compiler.ast)
+  end
+  def test_multiple_all_varibles
+    all = compile ':a +:b - :c * :d / :e'
+    assert_eq 11, depth(@compiler.ast)
+  end
+  def test_mix_of_variables_and_constant_exprs
+    compile ':a + :b + 5 + 8 * 10'
+    assert_eq 7, depth(@compiler.ast)
   end
 end
