ee8446e3e550bca83f1ffbeda6c0958b5bd19160
edhowland <ed.howland@gmail.com>
2018-09-18 15:54:17 -0400

    \'added helper methods last_child?/last_child to TailCall in lib/analysis/tail_call.rb\'

--- lib/analysis/tail_call.rb
+++ lib/analysis/tail_call.rb
@@ -1,13 +1,41 @@
 # tail_call.rb - class TailCall - TCO optimization
 
 class TailCall
+  include ListProc
   def run(ast)
     ast
+  end
+  # helper methods
+  def last_child?(ast)
+    !undefined?(cdr(ast)) && null?(cdr(ast))
+  end
+  def last_child(ast)
+    if null?(ast)
+      NullType.new
+    elsif last_child?(ast)
+      ast
+    else
+      last_child(cdr(ast))
+    end
+  end
+  def lambdacall?(sexp)
+    car(sexp) == :lambdacall
+  end
+  def lambda?(ast)
+    car(ast) == :lambda
+  end
+  def formals(ast)
+    cadr(ast)
+  end
+  def body(ast)
+    caddr(ast)
   end
 
   def _run(ast)
     if null?(ast)
       NullType.new
+    elsif lambda?(ast)
+      list(:lambda, formals(ast), body(ast))
     elsif pair?(car(ast))
       cons(_run(car(ast) ), _run(cdr(ast)))
 
