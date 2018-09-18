244d8be9b221219a6ea6128d5d4c2000e091824d
edhowland <ed.howland@gmail.com>
2018-09-18 16:22:43 -0400

    \'wishful thinking version of TailCall._run\'

--- lib/analysis/tail_call.rb
+++ lib/analysis/tail_call.rb
@@ -13,7 +13,7 @@ class TailCall
     if null?(ast)
       NullType.new
     elsif last_child?(ast)
-      ast
+      car(ast)
     else
       last_child(cdr(ast))
     end
@@ -30,12 +30,25 @@ class TailCall
   def body(ast)
     caddr(ast)
   end
+  # tail_candidate? body -  true if last_child(body) is a lambdacall or ...
+  def tail_candidate?(ast)
+    lambdacall?(last_child(ast))
+  end
+
+  # compose_block  body - handle last child because it is in tail position
+  def compose_block(body)
+    body
+  end
 
   def _run(ast)
     if null?(ast)
       NullType.new
     elsif lambda?(ast)
-      list(:lambda, formals(ast), body(ast))
+      if tail_candidate?(body(ast))
+        list(:lambda, formals(ast), compose_block(body(ast)))
+      else
+        list(:lambda, formals(ast), body(ast))
+      end
     elsif pair?(car(ast))
       cons(_run(car(ast) ), _run(cdr(ast)))
 
