b0d90d413c39d5a9126e8f885311ac63b749a9ee
Ed Howland <ed.howland@gmail.com>
2018-11-06 15:09:53 -0500

    simple creation of ObjectType, PairType and LambdaType in ffi_types.rb

--- ffi_types.rb
+++ ffi_types.rb
@@ -4,4 +4,17 @@ module FFITypes
   def mkvector(*args)
     args
   end
+
+  def mkpair(key, value)
+        PairType.new(key:key, value:value)
+  end
+  def mkobject(*args)
+        ObjectFactory.build(args)
+  end
+
+  # _mklambda - Use to create new LambdaType
+    def _mklambda(parms, body, id, loc=nil)
+    result = LambdaType.new(parms:parms, body:body, _binding:@vm.ctx.vars(), loc:loc)
+
+  end
 end
